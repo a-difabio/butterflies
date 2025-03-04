df4ed093d70e6ab6d3eeb4ed7705cf2be7930f7e
a-difabio <a.difabio@protonmail.com>
2025-03-04 16:04:31 +0100

    update metadata fields

--- metadata.py
+++ metadata.py
@@ -15,28 +15,28 @@ class Butterfly():
         self.family = row[0]
         self.genus = row[1]
         self.species = row[2]
-        self.subspecies = row[3]
-        self.higher_classification = row[4]
-        self.sex = row[5]
-        self.latitude = row[6]
-        self.longitude = row[7]
-        self.country = row[8]
-        self.name = row[9]
-        self.name_author = row[10]
-        self.day = row[11]
-        self.month = row[12]
-        self.year = row[13]
-        self.id = int(row[14])
-        self.occurence_id = row[15]
-        self.image_id = row[16]
+        # self.subspecies = row[3]
+        self.higher_classification = row[3]
+        self.sex = row[4]
+        self.latitude = row[5]
+        self.longitude = row[6]
+        self.country = row[7]
+        # self.name = row[9]
+        # self.name_author = row[10]
+        # self.day = row[11]
+        # self.month = row[12]
+        self.year = row[8]
+        self.id = int(row[9])
+        self.occurence_id = row[10]
+        self.image_id = row[11]
 
     @property
     def image_filename(self):
-        return 'data/raw/{:s}.jpg'.format(self.image_id)
+        return 'data/raw/{!s}.jpg'.format(self.id)
 
     @property
     def image_url(self):
-        return 'https://www.nhm.ac.uk/services/media-store/asset/{:s}/contents/preview'.format(self.image_id)
+        return format(self.image_id)
 
     @property
     def pretty_time(self):
@@ -72,7 +72,7 @@ def load():
     result = []
 
     row_index = 0
-    progress = tqdm(total=727248, desc='Reading metadata.csv')
+    progress = tqdm(total=8435, desc='Reading metadata.csv')
 
     for row in reader_iterator:
         result.append(Butterfly(row))
